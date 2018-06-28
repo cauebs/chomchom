710c4bb41064ab030e468567b3b47a5f71c460cc
Cauê Baasch de Souza <cauebs@pm.me>
2018-06-27 22:48:40 -0300

    Add format_first and format_follow to utils

--- chomchom/utils.py
+++ chomchom/utils.py
@@ -5,3 +5,17 @@ def powerset(iterable):
     s = list(iterable)
     return chain.from_iterable(combinations(s, r)
                                for r in range(0, len(s)+1))
+
+
+def format_first(grammar) -> str:
+    return '\n'.join(
+        f"first({str(nt)}) = {{{', '.join(str(f) for f in firsts)}}}"
+        for nt, firsts in grammar.first.items()
+    )
+
+
+def format_follow(grammar) -> str:
+    return '\n'.join(
+        f"follow({str(nt)}) = {{{', '.join(str(f) for f in follows)}}}"
+        for nt, follows in grammar.follow.items()
+    )
